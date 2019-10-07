66838a579d23c8b961c4da764d3df4c3893318fa
kamilkusmierz <kusmierz.kamil@o2.pl>
2019-10-07 19:02:48 +0200

    Test

--- src/main/java/com/RestServer/jwtauthentication/auth/AuthControler.java
+++ src/main/java/com/RestServer/jwtauthentication/auth/AuthControler.java
@@ -4,7 +4,7 @@ import com.RestServer.jwtauthentication.message.request.LoginForm;
 import com.RestServer.jwtauthentication.message.request.SignUpForm;
 import com.RestServer.jwtauthentication.message.response.JwtResponse;
 import com.RestServer.jwtauthentication.message.response.ResponseMessage;
-
+//Komentarz
 public interface AuthControler {
 
     public JwtResponse authenticateUser(LoginForm loginRequest);
